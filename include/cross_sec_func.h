b3387577637fd6267405e8a8bec5ec866d55a3cb
pashax <pashax@DESKTOP-4R27AAS>
2023-12-19 01:45:12 +0300

    removed unnecessary includes

--- include/cross_sec_func.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <TH2D.h>
-#include "LHAPDF/LHAPDF.h"
-
-
-void dsigmadptdy_QCDCompton(double s, double alpha, const LHAPDF::PDF *pdf, const std::vector<int> quark_pids, TH2D *outputHist);
